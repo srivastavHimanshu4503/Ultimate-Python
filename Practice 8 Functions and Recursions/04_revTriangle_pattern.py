80e443d871df450fbb83dffac100ff7cd759cadd
Himanshu <codeforinterviewwithhimanshu@gmail.com>
2024-09-24 00:18:35 +0530

    Implement new methods to perform same operations

--- Practice 8 Functions and Recursions/04_revTriangle_pattern.py
+++ Practice 8 Functions and Recursions/04_revTriangle_pattern.py
@@ -5,10 +5,25 @@ for n = 3
 ** 
 *
 """
+# Method 1 - Using one for loop 
+# def print_rev_triangle(N):
+#     for i in range(N):
+#         print("*"*(N-i), end="")
+#         print()
+
+# Method 2 - Using two for loop
+# def print_rev_triangle(N):
+#     for i in range(N):
+#         for j in range(N-i):
+#             print("*", end="")
+#         print()
+
+# Method 3 - Using Recusion
 def print_rev_triangle(N):
-    for i in range(N):
-        print("*"*(N-i), end="")
-        print()
+    if N==0:
+        return
+    print("*"*N)
+    print_rev_triangle(N-1)
+
 
 print_rev_triangle(3)
-print_rev_triangle(5)
